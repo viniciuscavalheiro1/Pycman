8d7de9d431c29c8414a6aaa446ea1ca736d4d726
Vinicius Loiola <viniciuspbu@gmail.com>
2018-08-29 13:37:49 -0300

    Inclusão dos Primeiros Testes

--- SistemaBancario.py
+++ SistemaBancario.py
@@ -1,4 +1,5 @@
-"""Toda modificão feita, informe com comentários."""
+u"""Toda modificão feita, informe com comentários."""
+
 
 class Pessoa:
     """Classe para guardar dados das pessoas."""
@@ -24,9 +25,13 @@ class Pessoa:
         """Atualiza CPF."""
         self.__cpf = cpf
 
-class Conta:
-    def __init__(self):
-        """Dados da conta.."""
+
+class Conta(Pessoa):
+    """Recebendo os dados e passando pelo construtor."""
+
+    def __init__(self, nome, cpf):
+        """Dados da conta."""
+        self.__init__(nome, cpf)
         self.__num_conta = ''
         self.__senha = ''
         self.__saldo = 0
@@ -50,3 +55,38 @@ class Conta:
     def set_saldo(self, saldo):
         """Atualiza Saldo."""
         self.__saldo = saldo
+
+
+class Contato(Conta):
+    """Dados do contato."""
+
+    def __init__(self, nome, cpf, num_telefone, email, endereco):
+        """Passando os dados pelo construtor e recebendo os dados."""
+        self.__init__(nome, cpf)
+        self.num_telefone = num_telefone
+        self.email = email
+        self.endereco = endereco
+
+    def get_num_telefone(self):
+        """Retorna o numero telefone."""
+        return self.num_telefone
+
+    def set_num_telefone(self, num_telefone):
+        """Atualiza telefone."""
+        self.num_telefone = num_telefone
+
+    def get_email(self):
+        """Retorna email."""
+        return self.email
+
+    def set_email(self, email):
+        """Atualiza email."""
+        self.email = email
+
+    def get_endereco(self):
+        u"""Retorna endereço."""
+        return self.endereco
+
+    def set_endereco(self, endereco):
+        u"""Atualiza endereço."""
+        self.endereco = endereco
