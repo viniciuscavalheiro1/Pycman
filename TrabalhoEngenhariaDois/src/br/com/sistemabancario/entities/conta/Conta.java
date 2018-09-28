37884049ec443c1353fe68a90bbf65d3b5c6e327
Denilson Sousa <denilsondssufpi@gmai.com>
2018-09-28 12:15:09 -0300

    Estrutura inicial do projeto (Arvore) + algumas classes criadas

---
+++ TrabalhoEngenhariaDois/src/br/com/sistemabancario/entities/conta/Conta.java
@@ -0,0 +1,12 @@
+package br.com.sistemabancario.entities.conta;
+
+public class Conta {
+
+	 protected int numConta;
+	 protected String nome;
+	 protected float saldo;
+	 protected String senha;
+	 protected int tipo;
+	 
+	/* ADICIONAR O CONSTRUTOR E GETTERS AND SETTERS*/
+}
---
+++ TrabalhoEngenhariaDois/src/br/com/sistemabancario/entities/conta/Conta_Poupanca.java
@@ -0,0 +1,6 @@
+package br.com.sistemabancario.entities.conta;
+
+public class Conta_Poupanca extends Conta{
+
+	
+}
