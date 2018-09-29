f98e359eb96b88ea80fc452d5da357e6cf8434c4
Vinicius Loiola <viniciuspbu@gmail.com>
2018-09-28 23:45:14 -0300

    Remoção dos arquivos python do projeto

--- TrabalhoEngenhariaDois/src/br/com/sistemabancario/entities/conta/Conta.java
+++ TrabalhoEngenhariaDois/src/br/com/sistemabancario/entities/conta/Conta.java
@@ -2,11 +2,17 @@ package br.com.sistemabancario.entities.conta;
 
 public class Conta {
 
-	 protected String numConta;
-	 protected String nome;
-	 protected float saldo;
-	 protected String senhaCad;
-	 protected int tipo;
+	 private String numConta;
+	 private String nome;
+	 private float saldo;
+	 private String senhaCad;
+	 private int tipo;
+	 //Para que esse int tipo
+	 
+	 Conta(String numConta, String nome, 0, String senhaCad) {
+		 this.numConta = numConta;
+		 this.
+	 }
 	 
 	
 	@Override
